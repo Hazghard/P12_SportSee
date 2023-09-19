92c01e5eabb766dda358f678eef5362eca704b20
Hazghard <100840855+Hazghard@users.noreply.github.com>
2023-09-19 17:48:18 +0200

    Delete src directory

--- src/_Services/MockedDataConstruct.service.ts
+++
@@ -1,7 +0,0 @@
-// Notre backend doit mettre en place une commande api permettant de recuperer une liste de tous les ID utilisateurs
-
-// Ensuite ce service permettra d'executer une requete par user, gather les data et les push dans un mock temporaire
-// 			Mock temporaire pourne pas ecraser les data du mock en cour en cas de fail en plain milieu de la recup de toutes les datas
-
-// Une fois l'ensemble des data recupéré, on vien ecraser les data anciennement mockées par les nouvelles
-// On supprime le fichier temporaire
